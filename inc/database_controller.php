6866b364e51cd55dcf0ab778b7ffb4820fcffd65
Gergo Birkas <44745172+AntaresOr@users.noreply.github.com>
2019-11-04 20:08:45 +0100

    Create database_controller.php

---
+++ inc/database_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+#Open a connection to the MySQL database with the username, password and database name specified
+function openSQLConn(){
+	$conn = mysqli_connect("localhost", "root", "", "projekter");
+	
+	//If the connection could not be established report accordingly
+	if (!$conn) {
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		return false;
+	}
+	
+	return $conn;
+}
+
+#Close connection to MySQL database
+function closeSQLConn($conn){
+	mysqli_close($conn);
+}
+
+?>
