a1c698929466cd5b3ef466903108ffd541eecb6a
Gergo Birkas <44745172+AntaresOr@users.noreply.github.com>
2019-11-04 20:11:09 +0100

    Create decrypt.php

---
+++ security/decrypt.php
@@ -0,0 +1,13 @@
+<?php
+	
+  function decrypt($data){
+    $private_key = file_get_contents("./security/mykey.pem");
+    if (openssl_private_decrypt(base64_decode($data), $decrypted, $private_key))
+      $data = $decrypted;
+    else
+      $data = '';
+      
+    return $data;
+    }
+    
+?>
